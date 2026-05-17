32a7f1bc3d4d553ef982865eaad2cfb7a8d095f2
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2026-05-17 18:12:21 +0530

    𝚴 𝐎 𝐁 𝚰 𝐓 𝚲

--- NOBITA_MUSIC/plugins/yumi/wishcute.py
+++ NOBITA_MUSIC/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from NOBITA_MUSIC import app 
 
-SUPPORT_CHAT = "NOB1TA_SUPPORT"
+SUPPORT_CHAT = "NOBITA_SUPP0RT"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
