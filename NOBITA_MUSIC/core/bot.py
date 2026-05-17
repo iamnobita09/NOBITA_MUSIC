57df73b42ee2772f777406cb601cec9ea64b1aec
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2026-05-17 23:29:24 +0530

    𝚴 𝐎 𝐁 𝚰 𝐓 𝚲

--- NOBITA_MUSIC/core/bot.py
+++ NOBITA_MUSIC/core/bot.py
@@ -1,45 +1,51 @@
 from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus
 
 import config
 
 from ..logging import LOGGER
 
-
 class NOBITA(Client):
-    def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
-        super().__init__(
-            name="NOBITA_MUSIC",
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
-            bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            max_concurrent_transmissions=7,
-        )
+def init(self):
+LOGGER(name).info(f"Starting Bot...")
+super().init(
+name="NOBITA_MUSIC",
+api_id=config.API_ID,
+api_hash=config.API_HASH,
+bot_token=config.BOT_TOKEN,
+in_memory=True,
+max_concurrent_transmissions=7,
+)
 
-    async def start(self):
-        await super().start()
-        self.id = self.me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.username = self.me.username
-        self.mention = self.me.mention
+async def start(self):  
+    await super().start()  
+    self.id = self.me.id  
+    self.name = self.me.first_name + " " + (self.me.last_name or "")  
+    self.username = self.me.username  
+    self.mention = self.me.mention  
 
-        try:
-            await self.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
-            )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel."
-            )
+    try:  
+        await self.send_message(  
+            chat_id=config.LOGGER_ID,  
+            text=f"<u><b>» {self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",  
+        )  
+    except (errors.ChannelInvalid, errors.PeerIdInvalid):  
+        LOGGER(__name__).error(  
+            "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."  
+        )  
 
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\nReason : {type(ex).__name__}."
-            )
+    except Exception as ex:  
+        LOGGER(__name__).error(  
+            f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."  
+        )  
 
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+    a = await self.get_chat_member(config.LOGGER_ID, self.id)  
+    if a.status != ChatMemberStatus.ADMINISTRATOR:  
+        LOGGER(__name__).error(  
+            "Please promote your bot as an admin in your log group/channel."  
+        )  
 
-    async def stop(self):
-        await super().stop()
+    LOGGER(__name__).info(f"Music Bot Started as {self.name}")  
+
+async def stop(self):  
+    await super().stop()
