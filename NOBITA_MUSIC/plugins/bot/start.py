ae458c2bef2da0fb0a895c7c0ce3d86380a09d6f
𝚴 𝐎 𝐁 𝚰 𝐓 𝚲 <nobitaxmusicbot@gmail.com>
2026-05-17 17:57:11 +0530

    𝚴 𝐎 𝐁 𝚰 𝐓 𝚲

--- NOBITA_MUSIC/plugins/bot/start.py
+++ NOBITA_MUSIC/plugins/bot/start.py
@@ -36,6 +36,7 @@ NEXI_VID = [
 "https://graph.org/file/318eac81e3d4667edcb77.mp4",
 "https://graph.org/file/7c1aa59649fbf3ab422da.mp4",
 "https://files.catbox.moe/t0nepm.mp4",
+"https://files.catbox.moe/s7e5tu.mp4",
 
 ]
 
